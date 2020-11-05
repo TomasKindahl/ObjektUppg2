165d9a394f95a920dfa47ff926cbad11487dd2c9
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-05 12:17:02 +0100

    Uppg 1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,36 @@ namespace ObjektUppg2
 {
     class Program
     {
+        class DictEntry
+        {
+            public string swedish, english;
+            public DictEntry(string eng, string swe)
+            {
+                swedish = swe; english = eng;
+            }
+        }
         static void Main(string[] args)
         {
+            DictEntry[] dict = new DictEntry[30];
+            dict[0] = new DictEntry("head", "huvud");
+            dict[1] = new DictEntry("see", "se");
+            dict[2] = new DictEntry("father", "far, pappa");
+            dict[3] = new DictEntry("tree", "träd");
+            dict[4] = new DictEntry("moon", "måne");
+            dict[5] = new DictEntry("move", "flytta");
+            dict[6] = new DictEntry("city", "stad");
+            Console.WriteLine("{0,-10}{1,-20}",
+                              "english", "swedish");
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < dict.Length; i++)
+            {
+                if (dict[i] != null)
+                {
+                    Console.WriteLine("{0,-10}{1,-20}",
+                                      dict[i].english, dict[i].swedish);
+                }
+            }
+            Console.WriteLine("-------------------");
         }
     }
 }
